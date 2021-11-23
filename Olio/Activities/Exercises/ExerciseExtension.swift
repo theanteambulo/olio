2fdc289715cd30d7f078e74432d68492bae9e3fd
Jake <jake@rnsmeals.com>
2021-11-23 18:16:55 +0000

    Created extensions on Exercise and Workout to make handling of Core Data optionality easier.

--- Olio/Activities/Exercises/ExerciseExtension.swift
+++ Olio/Activities/Exercises/ExerciseExtension.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension Exercise {
+    var exerciseName: String {
+        name ?? ""
+    }
+
+    static var example: Exercise {
+        let dataController = DataController(inMemory: true)
+        let viewContext = dataController.container.viewContext
+
+        let exercise = Exercise(context: viewContext)
+        exercise.name = "Example Exercise"
+        exercise.bodyweight = Bool.random()
+        exercise.muscleGroup = Int16.random(in: 1...7)
+        exercise.reps = 10
+        exercise.sets = 3
+
+        return exercise
+    }
+}
--- Olio/Activities/Exercises/WorkoutExtension.swift
+++ Olio/Activities/Exercises/WorkoutExtension.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+extension Workout {
+    var workoutName: String {
+        name ?? ""
+    }
+
+    var workoutDateScheduled: Date {
+        dateScheduled ?? Date()
+    }
+
+    var workoutDateCompleted: Date {
+        dateCompleted ?? Date()
+    }
+
+    var workoutExercises: [Exercise] {
+        let exercisesArray = exercises?.allObjects.first as? [Exercise] ?? []
+        return exercisesArray
+    }
+
+    static var example: Workout {
+        let dataController = DataController(inMemory: true)
+        let viewContext = dataController.container.viewContext
+
+        let workout = Workout(context: viewContext)
+        workout.name = "Example Workout"
+        workout.template = Bool.random()
+        workout.dateScheduled = Date()
+        workout.dateCompleted = Date()
+        workout.completed = true
+
+        return workout
+    }
+}
