9666e9036e9eafc91aec9585334c6a657b92f928
Jake <jake@rnsmeals.com>
2021-11-25 20:28:44 +0000

    Added functionality for users to be able to add items.

--- Olio/Activities/Exercises/AddExerciseView.swift
+++ Olio/Activities/Exercises/AddExerciseView.swift
@@ -9,17 +9,67 @@ import SwiftUI
 
 struct AddExerciseView: View {
     @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @Environment(\.dismiss) var dismiss
 
     @State private var name = ""
     @State private var bodyweight = true
     @State private var muscleGroup = 1
 
+    var saveToolbarItem: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Button("Save") {
+                save()
+                dismiss()
+            }
+        }
+    }
+
+    var dismissNoSaveToolbarItem: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button {
+                dismiss()
+            } label: {
+                Label("Dismiss", systemImage: "xmark")
+            }
+        }
+    }
+
     var body: some View {
         NavigationView {
             Form {
-//                Section(header: Text())
+                Section(header: Text("Basic Settings")) {
+                    TextField("Exercise name", text: $name)
+
+                    Picker("Muscle Group", selection: $muscleGroup) {
+                        Text("Chest").tag(1)
+                        Text("Back").tag(2)
+                        Text("Shoulders").tag(3)
+                        Text("Biceps").tag(4)
+                        Text("Triceps").tag(5)
+                        Text("Legs").tag(6)
+                        Text("Abs").tag(7)
+                    }
+
+                    Toggle("Bodyweight exercise", isOn: $bodyweight)
+                }
+            }
+            .navigationTitle("Add Exercise")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                saveToolbarItem
+                dismissNoSaveToolbarItem
             }
         }
+    }
+
+    func save() {
+        let exercise = Exercise(context: managedObjectContext)
+        exercise.name = name
+        exercise.muscleGroup = Int16(muscleGroup)
+        exercise.bodyweight = bodyweight
+
+        dataController.save()
     }
 }
 
--- Olio/Activities/Exercises/ExercisesView.swift
+++ Olio/Activities/Exercises/ExercisesView.swift
@@ -43,7 +43,7 @@ struct ExercisesView: View {
                 Label("", systemImage: "plus")
             }
             .sheet(isPresented: $showingAddExerciseSheet) {
-                EmptyView()
+                AddExerciseView()
             }
         }
     }
