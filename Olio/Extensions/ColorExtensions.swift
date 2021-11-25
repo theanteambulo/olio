c792e158c428e26403f044d9b1eef6d9459b64e7
Jake <jake@rnsmeals.com>
2021-11-25 08:04:39 +0000

    Extension on Color to add system grouped background colours to SwiftUI Color view.

---
+++ Olio/Extensions/ColorExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtensions.swift
+//  Olio
+//
+//  Created by Jake King on 25/11/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
