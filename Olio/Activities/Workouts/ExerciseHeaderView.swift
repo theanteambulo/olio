8773dbdbb306a728ed4b65ffc1204709aaa47590
Jake <jake@rnsmeals.com>
2021-12-11 12:38:25 +0000

    ExerciseHeaderView.swift

--- Olio/Activities/Workouts/ExerciseHeaderView.swift
+++ Olio/Activities/Workouts/ExerciseHeaderView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
+/// A header for a given exercise in a given workout representing the percentage of exercise sets completed so far.
 struct ExerciseHeaderView: View {
+    /// The workout used to construct this view.
     @ObservedObject var workout: Workout
+
+    /// The exercise used to construct this view.
     @ObservedObject var exercise: Exercise
 
+    /// The percentage of exercises sets completed, expressed as an integer.
     var exerciseCompletionAmountInt: Int {
         Int(100 * exerciseCompletionAmount(exercise))
     }
@@ -25,6 +30,11 @@ struct ExerciseHeaderView: View {
         .accessibilityLabel(Text("Progress: \(exerciseCompletionAmountInt)%"))
     }
 
+    /// Calculates the percentage of exercise sets completed for a given exercise.
+    ///
+    /// Note a Double is returned as ProgressView requires a BinaryFloatingPoint type.
+    /// - Parameter exercise: The exercise parent to the exercise sets.
+    /// - Returns: A Double representing the percentage of exercises sets completed.
     func exerciseCompletionAmount(_ exercise: Exercise) -> Double {
         let allSets = exercise.exerciseSets.filter { $0.workout == workout }
         guard allSets.isEmpty == false else { return 0 }
