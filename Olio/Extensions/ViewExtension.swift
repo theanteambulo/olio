35b6d43354b0097ba8ec8b92df5ada3265915ae1
Jake <jake@rnsmeals.com>
2021-12-11 09:58:15 +0000

    Documenting ViewExtension.swift

--- Olio/Extensions/ViewExtension.swift
+++ Olio/Extensions/ViewExtension.swift
@@ -9,6 +9,8 @@ import Foundation
 import SwiftUI
 
 extension View {
+    /// Ensures that the navigation view style used by the app is stacked when the user's device is a phone.
+    /// - Returns: The view with the navigationViewStyle() modifier applied.
     func phoneOnlyStackNavigationView() -> some View {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
