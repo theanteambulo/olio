f9e4b767d8fd78ca38cbd4836ea020e824d93e11
Jake <jake@rnsmeals.com>
2021-12-11 10:04:28 +0000

    Documenting ColorExtension.swift

--- Olio/Extensions/ColorExtension.swift
+++ Olio/Extensions/ColorExtension.swift
@@ -9,6 +9,9 @@ import Foundation
 import SwiftUI
 
 extension Color {
+    /// Wrapped UIKit UIColor value giving SwiftUI built-in semantic colour for systemGroupedBackground.
     static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+
+    /// Wrapped UIKit UIColor value giving SwiftUI built-in semantic colour for secondarySystemGroupedBackground.
     static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
 }
