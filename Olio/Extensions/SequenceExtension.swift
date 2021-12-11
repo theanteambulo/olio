c4a27fcfdd3bfbca669d12575addebe02355fbd1
Jake <jake@rnsmeals.com>
2021-12-11 09:53:58 +0000

    Documenting SequenceExtension.swift

--- Olio/Extensions/SequenceExtension.swift
+++ Olio/Extensions/SequenceExtension.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 extension Sequence {
+    /// Sorts generic objects using a specified key path and sorting method.
+    /// - Returns: A sorted array of elements of whichever type was specified in the key path.
     func sorted<Value>(by keyPath: KeyPath<Element, Value>,
                        using areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows -> [Element] {
         try self.sorted {
@@ -15,6 +17,8 @@ extension Sequence {
         }
     }
 
+    /// Sorts comparable objects using a specified key path.
+    /// - Returns: A sorted array of elements of whichever type was specified in the key path.
     func sorted<Value: Comparable>(by keyPath: KeyPath<Element, Value>) -> [Element] {
         self.sorted(by: keyPath, using: <)
     }
