4b93df58399b2f9f4821f81e54a3509ba542fced
Jake <jake@rnsmeals.com>
2021-12-11 10:07:35 +0000

    Documenting BindingExtension.swift

--- Olio/Extensions/BindingExtension.swift
+++ Olio/Extensions/BindingExtension.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 extension Binding {
+    /// Calls a handler method when a binding's value is changed.
+    /// - Parameter handler: The method to call on change of the binding's value.
+    /// - Returns: A new instance of Binding that uses the same type of data as the original binding.
     func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
         Binding(
             get: { self.wrappedValue },
