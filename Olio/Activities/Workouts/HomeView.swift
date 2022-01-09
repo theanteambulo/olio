e0c2883ccfa3d598a35d0b5c62d1d0cf48b86198
Jake <jake@rnsmeals.com>
2022-01-09 17:13:47 +0000

    Refactored HomeView to add swipeActions to the list of workouts scheduled and completed.

--- Olio/Activities/Workouts/HomeView.swift
+++ Olio/Activities/Workouts/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  NewHomeView.swift
+//  HomeView.swift
 //  Olio
 //
 //  Created by Jake King on 09/01/2022.
@@ -11,15 +11,24 @@ import SwiftUI
 ///
 /// Note templates will only be displayed when the showingCompletedWorkouts Boolean is set to false. When this is the
 /// case, the list of workouts displayed will be conditional on their completed property value.
-struct NewHomeView: View {
+struct HomeView: View {
     /// The presentation model representing the state of this view capable of reading model data and carrying out all
     /// transformations required to prepare that data for presentation.
     @StateObject var viewModel: ViewModel
+
+    /// Boolean to indicate whether a confirmation dialog is being shown. Toggled when a user swipes to reschedule.
+    @State private var showingSwipeToRescheduleDialog = false
 
     /// Tag value for the "Home" tab.
     static let homeTag: String? = "Home"
     /// Tag value for the "History" tab.
     static let historyTag: String? = "History"
+
+    init(dataController: DataController, showingCompletedWorkouts: Bool) {
+        let viewModel = ViewModel(dataController: dataController,
+                              showingCompletedWorkouts: showingCompletedWorkouts)
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
 
     /// Computed property to get the text displayed in the navigation title of the view.
     var navigationTitleLocalizedStringKey: Text {
@@ -37,32 +46,67 @@ struct NewHomeView: View {
         }
     }
 
+    /// Computed property that derives the label of the button used to change the completion status of a workout when
+    /// the user swipes from the leading edge.
+    var swipeToToggleCompletionStatusLabel: some View {
+        if viewModel.showingCompletedWorkouts {
+            return Label(Strings.incompleteButton.localized, systemImage: "xmark.circle")
+                .labelStyle(.titleAndIcon)
+        } else {
+            return Label(Strings.completeButton.localized, systemImage: "checkmark.circle")
+                .labelStyle(.titleAndIcon)
+        }
+    }
+
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
                 // Display templates if on the "Home" tab.
-                if !showingCompletedWorkouts {
-                    TemplatesView(dataController: dataController)
+                if !viewModel.showingCompletedWorkouts {
+                    TemplatesView(dataController: viewModel.dataController)
                 }
 
                 List {
                     // Add a new workout - displayed only when not viewing completed workouts.
-                    if !showingCompletedWorkouts {
+                    if !viewModel.showingCompletedWorkouts {
                         Button {
                             withAnimation {
-                                addWorkout()
+                                viewModel.addWorkout()
                             }
                         } label: {
                             Label(Strings.newWorkout.localized, systemImage: "plus")
                         }
                     }
 
-                    ForEach(workoutDates, id: \.self) { date in
+                    ForEach(viewModel.workoutDates.sorted(), id: \.self) { date in
                         Section(header: Text("\(date.formatted(date: .complete, time: .omitted))")) {
                             // Display list of workouts, if any exist.
-                            WorkoutsListView(dataController: dataController,
-                                             showingCompletedWorkouts: showingCompletedWorkouts,
-                                             workoutsOnDate: date)
+                            ForEach(viewModel.filterWorkouts(viewModel.workouts, by: date), id: \.self) { workout in
+                                WorkoutRowView(workout: workout)
+                                    .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                                        // Complete/incomplete workout
+                                        Button {
+                                            withAnimation {
+                                                viewModel.toggleCompletionStatusForWorkout(workout)
+                                            }
+                                        } label: {
+                                            swipeToToggleCompletionStatusLabel
+                                        }
+                                        .tint(workout.completed ? .orange : .green)
+                                    }
+                                    .swipeActions(edge: .trailing) {
+                                        // Delete workout
+                                        Button {
+                                            withAnimation {
+                                                viewModel.deleteWorkout(workout)
+                                            }
+                                        } label: {
+                                            Label(Strings.deleteButton.localized, systemImage: "trash")
+                                                .labelStyle(.titleAndIcon)
+                                        }
+                                        .tint(.red)
+                                    }
+                            }
                         }
                     }
                 }
@@ -79,6 +123,6 @@ struct NewHomeView: View {
 
 struct NewHomeView_Previews: PreviewProvider {
     static var previews: some View {
-        NewHomeView()
+        HomeView(dataController: DataController.preview, showingCompletedWorkouts: false)
     }
 }
--- Olio/Activities/Workouts/HomeViewModel.swift
+++ Olio/Activities/Workouts/HomeViewModel.swift
@@ -5,4 +5,111 @@
 //  Created by Jake King on 09/01/2022.
 //
 
+import CoreData
 import Foundation
+
+extension HomeView {
+    /// A presentation model representing the state of WorkoutsListView capable of reading model data and carrying out
+    /// all transformations needed to prepare that data for presentation.
+    class ViewModel: NSObject, ObservableObject, NSFetchedResultsControllerDelegate {
+        /// The environment singleton responsible for managing the Core Data stack.
+        let dataController: DataController
+        /// Boolean indicating whether completed or scheduled workouts should be displayed.
+        let showingCompletedWorkouts: Bool
+
+        /// Performs the initial fetch request and ensures it remains up to date.
+        private var workoutsController: NSFetchedResultsController<Workout>
+
+        /// An array of Workout objects.
+        @Published var workouts = [Workout]()
+        /// An array Date objects.
+        @Published var workoutDates = [Date]()
+
+        init(dataController: DataController,
+             showingCompletedWorkouts: Bool) {
+            self.dataController = dataController
+            self.showingCompletedWorkouts = showingCompletedWorkouts
+
+            // Initialise the fetch request.
+            let workoutsRequest: NSFetchRequest<Workout> = Workout.fetchRequest()
+
+            // Get all non-template workouts with completion status corresponding to showingCompletedWorkouts.
+            let templatePredicate = NSPredicate(format: "template != true")
+            let completionPredicate = NSPredicate(format: "completed = %d", showingCompletedWorkouts)
+            workoutsRequest.predicate = NSCompoundPredicate(type: .and,
+                                                            subpredicates: [templatePredicate,
+                                                                            completionPredicate])
+            // Sort workouts by date, then by name.
+            workoutsRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Workout.date, ascending: true)]
+
+            // Limit scheduled workouts displayed to 99.
+            if !showingCompletedWorkouts {
+                workoutsRequest.fetchLimit = 10
+            }
+
+            workoutsController = NSFetchedResultsController(
+                fetchRequest: workoutsRequest,
+                managedObjectContext: dataController.container.viewContext,
+                sectionNameKeyPath: nil,
+                cacheName: nil
+            )
+
+            // Set the class as the delegate of the fetched results controller so it announces when the data changes.
+            super.init()
+            workoutsController.delegate = self
+
+            // Execute the fetch request and assign fetched objects to the workouts property.
+            do {
+                try workoutsController.performFetch()
+                workouts = workoutsController.fetchedObjects?.sorted(by: \.workoutDate) ?? []
+                workoutDates = workouts.map({ Calendar.current.startOfDay(for: $0.workoutDate) }).removingDuplicates()
+            } catch {
+                print("Failed to fetch workouts: \(error.localizedDescription)")
+            }
+        }
+
+        /// Notifies WorkoutsListView when the underlying array of workouts changes.
+        /// - Parameter controller: The controller that manages the results of the view model's Core Data fetch request.
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            workouts = workoutsController.fetchedObjects?.sorted(by: \.workoutDate) ?? []
+            workoutDates = workouts.map({ Calendar.current.startOfDay(for: $0.workoutDate) }).removingDuplicates()
+        }
+
+        /// Creates a new workout.
+        func addWorkout() {
+            let newWorkout = Workout(context: dataController.container.viewContext)
+            newWorkout.id = UUID()
+            newWorkout.name = nil
+            newWorkout.date = Date.now
+            newWorkout.createdDate = Date.now
+            newWorkout.completed = false
+            newWorkout.template = false
+            dataController.save()
+        }
+
+        /// Toggles the completion status of a given workout.
+        /// - Parameter workout: The workout whose completion status will be toggle.
+        func toggleCompletionStatusForWorkout(_ workout: Workout) {
+            workout.completed.toggle()
+            workout.objectWillChange.send()
+            dataController.save()
+        }
+
+        /// Deletes a given workout from the Core Data context.
+        /// - Parameter workout: The workout to delete.
+        func deleteWorkout(_ workout: Workout) {
+            dataController.delete(workout)
+            workout.objectWillChange.send()
+            dataController.save()
+        }
+
+        func filterWorkouts(_ workouts: [Workout], by date: Date) -> [Workout] {
+            let filteredWorkouts = workouts.filter({ Calendar.current.startOfDay(for: $0.workoutDate) == date })
+            return filteredWorkouts
+        }
+
+        func deleteAll() {
+            dataController.deleteAll()
+        }
+    }
+}
