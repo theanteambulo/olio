9a59fb01434db3b42484e3f0257ce8871f0a7e17
Jake <jake@rnsmeals.com>
2022-03-29 15:44:17 +0100

    New view to be used on the home page in WorkoutRowView

--- Olio/Activities/Workouts/WorkoutExerciseCategoriesLabelView.swift
+++ Olio/Activities/Workouts/WorkoutExerciseCategoriesLabelView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct WorkoutExerciseCategoriesLabelView: View {
+    @ObservedObject var workout: Workout
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Map all the exercises into an array of categories (strings)
+        let categories = workout.workoutExercises.map({ $0.exerciseCategory })
+        let colors = Exercise.allExerciseCategoryColors
+
+        switch categories.count {
+        case 2:
+            let freeWeights = Text(.weights).font(.caption).foregroundColor(colors["Free Weights"])
+            let divider = Text(" | ").font(.caption).foregroundColor(.secondary)
+            let bodyWeight = Text(.body).font(.caption).foregroundColor(colors["Bodyweight"])
+
+            return freeWeights + divider + bodyWeight
+        case 1:
+            let divider = Text(" | ").font(.caption).foregroundColor(.secondary)
+
+            if categories.contains("Free Weights") {
+                let freeWeights = Text(.weights).font(.caption).foregroundColor(colors["Free Weights"])
+                let bodyWeight = Text(.body).font(.caption).foregroundColor(.secondary)
+
+                return freeWeights + divider + bodyWeight
+            } else {
+                let freeWeights = Text(.weights).font(.caption).foregroundColor(.secondary)
+                let bodyWeight = Text(.body).font(.caption).foregroundColor(colors["Body"])
+
+                return freeWeights + divider + bodyWeight
+            }
+        default:
+            return Text(.weightsAndBody).font(.caption).foregroundColor(.secondary)
+        }
     }
 }
 
 struct WorkoutExerciseCategoriesLabelView_Previews: PreviewProvider {
     static var previews: some View {
-        WorkoutExerciseCategoriesLabelView()
+        WorkoutExerciseCategoriesLabelView(workout: Workout.example)
     }
 }
